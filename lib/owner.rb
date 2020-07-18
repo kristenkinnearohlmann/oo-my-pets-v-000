2a8802a66ca2a57fe311425cda4d6a1b4714c9d0
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-07-18 23:47:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+  attr_accessor :cats, :dogs
+  attr_reader :name, :species
+
+  @@owners = []
+
+# instance methods
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@owners << self
+    @cats = []
+    @dogs = []
+  end
+
+  def say_species()
+    "I am a #{species}."
+  end
+
+  def cats()
+    @cats
+  end
+
+  def dogs()
+    @dogs
+  end
+
+#class methods
+
+  def self.all()
+    @@owners
+  end
+
+  def self.count()
+    @@owners.count
+  end
+
+  def self.reset_all()
+    @@owners = []
+  end
+
 end
