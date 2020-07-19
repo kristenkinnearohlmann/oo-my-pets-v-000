e65e779271d4e764da11e59d7961a7e3c68e0266
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-07-19 00:04:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -48,8 +48,14 @@ class Owner
   end
 
   def sell_pets
-    @dogs.each {|dog| dog.mood = "nervous"}
-    @cats.each {|cat| cat.mood = "nervous"}
+    @dogs.each do |dog| {
+      dog.mood = "nervous"
+      dog.owner = nil
+    }
+    @cats.each do |cat| {
+      cat.mood = "nervous"
+      cat.owner = nil
+    }
   end
 
 #class methods
